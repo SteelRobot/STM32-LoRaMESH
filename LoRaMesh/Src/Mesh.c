628ad7e7f4804c9c8024d1f8d83b28f826b565b5
Andrew <alutchak@yandex.ru>
2026-03-11 19:21:08 +0300

    Test dev commit

--- LoRaMesh/Src/Mesh.c
+++ LoRaMesh/Src/Mesh.c
@@ -1,10 +1,14 @@
 #include "main.h"
 #include "Mesh.h"
 
-struct RoutingTable {
-
-};
-
+// Distance Vector - Router Advertisement
 struct Node {
-
+	uint8_t Destination;
+	uint8_t Next;
+	uint8_t Metric;
+	uint8_t Sequence;
 };
+
+// Dynamic Source Routing
+// No periodic beaconing
+// Node floods the network with a Route REQuest (RREQ)
