aa6b0bde1358c1548e7ce7cb388371a65910e513
SteelRobot <alutchak@yandex.ru>
2026-04-04 19:21:24 +0300

    FIXED the DMA buffer (finally). Fixed defines of sizes so the packet length is calculated properly Added a ping packet, called by typing the address and then 'ping'

--- LoRaMesh/Src/LoRa.c
+++ LoRaMesh/Src/LoRa.c
@@ -7,11 +7,11 @@
 
 #define COMMAND_READ_PREFIX 0xC1
 #define COMMAND_WRITE_PREFIX 0xC0
-#define SLEEP_TIME 1000
+#define SLEEP_TIME 150
 #define MODE_WORKING MODE_NORMAL
 
 #define RX_FINAL_SIZE 1024
-#define RX_SIZE 256
+#define RX_SIZE 64
 
 static GPIO_TypeDef *GPIOx_M0;
 static GPIO_TypeDef *GPIOx_M1;
@@ -25,10 +25,17 @@ UART_HandleTypeDef *LoRa_UART;
 UART_HandleTypeDef *COM_UART;
 RTC_HandleTypeDef *Mesh_RTC;
 
+uint8_t addh = 0;
+uint8_t addl = 0;
+uint8_t netid = 0;
+uint8_t reg0 = 0;
+uint8_t reg1 = 0;
+uint8_t reg2 = 0;
+uint8_t reg3 = 0;
+
 static uint8_t rx_final_buffer[RX_FINAL_SIZE];
 static uint8_t rx_buffer[RX_SIZE];
-static uint16_t rx_data_len = 0;
-static uint16_t indx_1 = 0, indx_2 = 0, rx_cplt = 0;
+static uint16_t indx_1 = 0, indx_2 = 0;
 static bool receiving_config_data = false;
 
 static uint8_t command_buffer[4];
@@ -53,26 +60,54 @@ void LoRa_Init(UART_HandleTypeDef *huart1, UART_HandleTypeDef *huart2, RTC_Handl
 	PIN_AUX = AUX_Pin;
 
 	LoRa_Start_Receive();
+	LoRa_ReadRegister(ADDH, 8); //Reading all registers and parsing the reply as one function call
 
-	uint8_t addh = LoRa_ReadRegister(ADDH);
-	uint8_t addl = LoRa_ReadRegister(ADDL);
-	uint8_t netid = LoRa_ReadRegister(NETID);
-	uint8_t reg0 = LoRa_ReadRegister(REG0);
-	uint8_t reg1 = LoRa_ReadRegister(REG1);
-	my_channel = LoRa_ReadRegister(REG2);
-	uint8_t reg3 = LoRa_ReadRegister(REG3);
+	// It is recommended to set global LoRa parameters here or in the LoRa_init() from LoRa.c
+	// Address should be set up in the main.c, but that is in case that each MCU has a separate project of its own
+	// Otherwise just assign an address to one MCU, change it to another in main.c, and continue until you've programmed all the nodes
+	// START GLOBAL
+
+#ifdef DEBUG
+	LoRa_WriteRegister(REG1, 0x00);
+
+	LoRa_Set_Encryption(0xCAFE);
+
+	LoRa_Set_NetID(0xBF);
+
+	LoRa_Set_SerialPortRate(SPR_9600);
+	LoRa_Set_SerialParityBit(SPB_8N1);
+	LoRa_Set_AirDataRate(ADR_62_5K);
+
+	LoRa_Set_SubPacketSetting(SPS_240);
+	LoRa_Set_RSSIAmbientNoise(false);
+	LoRa_Set_TransmittingPower(TP_10DBM);
+
+	LoRa_Set_Channel(19);
+
+	LoRa_Set_RSSIEnable(false);
+	LoRa_Set_TransmissionMode(TM_FIXED_POINT);
+	LoRa_Set_ReplyEnable(false);
+	LoRa_Set_LBTEnable(true);
+	LoRa_Set_WORTransceiverControl(WOR_RECEIVER);
+	LoRa_Set_WORCycle(CYCLE_2000MS);
+#endif
+
+	// END GLOBAL
+
+	my_channel = reg2;
 	my_id = (addh << 8) + addl;
 
 #ifdef DEBUG
 	printf("Init LoRa Module:\n");
 	printf("ADDH =  0x%02X\n", addh);
 	printf("ADDL =  0x%02X\n", addl);
-	printf("Address = %d\n", my_id);
 	printf("NETID = 0x%02X\n", netid);
 	printf("REG0 =  0x%02X\n", reg0);
 	printf("REG1 =  0x%02X\n", reg1);
 	printf("REG2 =  0x%02X\n", my_channel);
 	printf("REG3 =  0x%02X\n", reg3);
+
+	printf("ID: %d, Channel: %d, NETID: %d\n", my_id, my_channel, netid);
 
 	printf("Init done.\n");
 #endif
@@ -124,21 +159,16 @@ void LoRa_ModeSelect(enum Mode mode) {
 // It was decided to let user only modify one register at a time, so length is set at 0x1
 
 // To return just the register's value
-uint8_t LoRa_ReadRegister(uint8_t address) {
+void LoRa_ReadRegister(uint8_t starting_address, uint8_t length) {
 	LoRa_ModeSelect(MODE_CONFIG);
 	receiving_config_data = true;
 	uint8_t command_index = 0;
 	command_buffer[command_index++] = COMMAND_READ_PREFIX;
-	command_buffer[command_index++] = address;
-	command_buffer[command_index++] = 0x1;
+	command_buffer[command_index++] = starting_address;
+	command_buffer[command_index++] = length;
 	HAL_UART_Transmit(LoRa_UART, command_buffer, command_index, SLEEP_TIME);
-	HAL_Delay(50);
+	HAL_Delay(SLEEP_TIME);
 	LoRa_ModeSelect(MODE_WORKING);
-	receiving_config_data = false;
-	if (indx_1 > 0)
-		return rx_buffer[indx_1 - 1];
-	else
-		return rx_buffer[RX_SIZE - 1];
 }
 
 void LoRa_WriteRegister(uint8_t address, uint8_t parameter) {
@@ -150,9 +180,8 @@ void LoRa_WriteRegister(uint8_t address, uint8_t parameter) {
 	command_buffer[command_index++] = 0x1;
 	command_buffer[command_index++] = parameter;
 	HAL_UART_Transmit(LoRa_UART, command_buffer, command_index, SLEEP_TIME);
+	HAL_Delay(SLEEP_TIME);
 	LoRa_ModeSelect(MODE_WORKING);
-	HAL_Delay(10);
-	receiving_config_data = false;
 }
 
 bool LoRa_SendData(uint8_t *buffer, uint8_t buffer_size) {
@@ -163,30 +192,28 @@ bool LoRa_SendData(uint8_t *buffer, uint8_t buffer_size) {
 // Register PID has 7 bytes
 void LoRa_ReadProductInfo(void) {
 	printf("Product info:\n");
-	for (int i = 0; i < 6; i++)
-		printf("0x%02X ", LoRa_ReadRegister(PID + i));
-	printf("0x%02X\n", LoRa_ReadRegister(PID));
+	LoRa_ReadRegister(PID, 7);
 }
 
 // Returns to factory default parameters
 // Frequency		- 869.125MHz
 // Address			- 0x0000
-// Channel			- 0x12
+// Channel			- 0x13
 // Air data rate	- 2.4kbps
 // Baud rate		- 9600
 // Parity format	- 8N1
 // Power			- 22dbm
 void LoRa_ResetFirmware(void) {
 	printf("Resetting firmware...\n");
-	LoRa_WriteRegister(ADDH, 0x0);
-	LoRa_WriteRegister(ADDL, 0x0);
-	LoRa_WriteRegister(NETID, 0x0);
+	LoRa_WriteRegister(ADDH, 0x00);
+	LoRa_WriteRegister(ADDL, 0x00);
+	LoRa_WriteRegister(NETID, 0x00);
 	LoRa_WriteRegister(REG0, 0x62);
 	LoRa_WriteRegister(REG1, 0x00);
 	LoRa_WriteRegister(REG2, 0x13);
 	LoRa_WriteRegister(REG3, 0x3);
-	LoRa_WriteRegister(CRYPT_H, 0x0);
-	LoRa_WriteRegister(CRYPT_L, 0x0);
+	LoRa_WriteRegister(CRYPT_H, 0x00);
+	LoRa_WriteRegister(CRYPT_L, 0x00);
 	printf("Reset done!\n");
 }
 
@@ -201,19 +228,19 @@ void LoRa_Set_NetID(uint8_t netid) {
 }
 // ADDR NETID SETS BEGIN
 void LoRa_Set_SerialPortRate(enum Serial_Port_Rate spr) {
-	uint8_t register_value = LoRa_ReadRegister(REG0) & 0b00011111;
+	uint8_t register_value = reg0 & 0b00011111;
 	uint8_t parameter = register_value | (spr << 5);
 	LoRa_WriteRegister(REG0, parameter);
 }
 
 void LoRa_Set_SerialParityBit(enum Serial_Parity_Bit spb) {
-	uint8_t register_value = LoRa_ReadRegister(REG0) & 0b11100111;
+	uint8_t register_value = reg0 & 0b11100111;
 	uint8_t parameter = register_value | (spb << 3);
 	LoRa_WriteRegister(REG0, parameter);
 }
 
 void LoRa_Set_AirDataRate(enum Air_Data_Rate adr) {
-	uint8_t register_value = LoRa_ReadRegister(REG0) & 0b11111000;
+	uint8_t register_value = reg0 & 0b11111000;
 	uint8_t parameter = register_value | adr;
 	LoRa_WriteRegister(REG0, parameter);
 }
@@ -221,19 +248,19 @@ void LoRa_Set_AirDataRate(enum Air_Data_Rate adr) {
 
 // REG1 SETS BEGIN
 void LoRa_Set_SubPacketSetting(enum Sub_Packet_Setting sps) {
-	uint8_t register_value = LoRa_ReadRegister(REG1) & 0b00111111;
+	uint8_t register_value = reg1 & 0b00111111;
 	uint8_t parameter = register_value | (sps << 6);
 	LoRa_WriteRegister(REG1, parameter);
 }
 
 void LoRa_Set_RSSIAmbientNoise(bool state) {
-	uint8_t register_value = LoRa_ReadRegister(REG1) & 0b11011111;
+	uint8_t register_value = reg1 & 0b11011111;
 	uint8_t parameter = register_value | (state << 5);
 	LoRa_WriteRegister(REG1, parameter);
 }
 
 void LoRa_Set_TransmittingPower(enum Transmitting_Power tp) {
-	uint8_t register_value = LoRa_ReadRegister(REG1) & 0b11111100;
+	uint8_t register_value = reg1 & 0b11111100;
 	uint8_t parameter = register_value | tp;
 	LoRa_WriteRegister(REG1, parameter);
 }
@@ -247,37 +274,37 @@ void LoRa_Set_Channel(uint8_t channel) {
 
 // REG3 SETS BEGIN
 void LoRa_Set_RSSIEnable(bool state) {
-	uint8_t register_value = LoRa_ReadRegister(REG3) & 0b01111111;
+	uint8_t register_value = reg3 & 0b01111111;
 	uint8_t parameter = register_value | (state << 7);
 	LoRa_WriteRegister(REG3, parameter);
 }
 
 void LoRa_Set_TransmissionMode(enum Transmission_Mode tm) {
-	uint8_t register_value = LoRa_ReadRegister(REG3) & 0b10111111;
+	uint8_t register_value = reg3 & 0b10111111;
 	uint8_t parameter = register_value | (tm << 6);
 	LoRa_WriteRegister(REG3, parameter);
 }
 
 void LoRa_Set_ReplyEnable(bool state) {
-	uint8_t register_value = LoRa_ReadRegister(REG3) & 0b11011111;
+	uint8_t register_value = reg3 & 0b11011111;
 	uint8_t parameter = register_value | (state << 5);
 	LoRa_WriteRegister(REG3, parameter);
 }
 
 void LoRa_Set_LBTEnable(bool state) {
-	uint8_t register_value = LoRa_ReadRegister(REG3) & 0b11101111;
+	uint8_t register_value = reg3 & 0b11101111;
 	uint8_t parameter = register_value | (state << 4);
 	LoRa_WriteRegister(REG3, parameter);
 }
 
 void LoRa_Set_WORTransceiverControl(enum WOR_Transceiver_Control state) {
-	uint8_t register_value = LoRa_ReadRegister(REG3) & 0b11110111;
+	uint8_t register_value = reg3 & 0b11110111;
 	uint8_t parameter = register_value | (state << 3);
 	LoRa_WriteRegister(REG3, parameter);
 }
 
 void LoRa_Set_WORCycle(enum WOR_Cycle cycle) {
-	uint8_t register_value = LoRa_ReadRegister(REG3) & 0b11111000;
+	uint8_t register_value = reg3 & 0b11111000;
 	uint8_t parameter = register_value | cycle;
 	LoRa_WriteRegister(REG3, parameter);
 }
@@ -300,23 +327,127 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
 }
 
 void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size) {
-	rx_data_len = Size;
+	// Copy new data from DMA circular buffer to final buffer
+	memcpy(rx_final_buffer + indx_2, rx_buffer + indx_1, Size);
 
-	if(!receiving_config_data)
-		memcpy(rx_final_buffer + indx_2, rx_buffer + indx_1, rx_data_len);
-
-	if (rx_data_len == RX_SIZE) {
-		indx_2 = RX_SIZE * ++rx_cplt;
+	// This part with calculating indexes gave me a LOT of pain
+	indx_2 += Size - indx_1;
+	if (Size == RX_SIZE) {
+#ifdef DEBUG
+	printf("Received POSSIBLY NOT-FULL %d bytes | Packet type: 0x%02X | Payload: %d\n", indx_2, rx_final_buffer[0], rx_final_buffer[1]);
+#endif
 		indx_1 = 0;
 	} else {
-		indx_2 = indx_2 + (rx_data_len - indx_1);
-		indx_1 = rx_data_len;
-
-		if (!receiving_config_data)
-			Receive_Packet_Handler(rx_final_buffer, indx_2);
-
-		memset(rx_final_buffer, 0, RX_FINAL_SIZE);
-		indx_2 = 0;
+#ifdef DEBUG
+	printf("Received %d bytes | Packet type: 0x%02X | Payload: %d\n", indx_2, rx_final_buffer[0], rx_final_buffer[1]);
+#endif
+		indx_1 = Size;
 	}
+
+    __disable_irq();
+	if (receiving_config_data) {
+		Process_LoRa_Reply();
+	} else {
+		Process_Packet();
+	}
+    __enable_irq();
+
+#ifdef DEBUG
+	printf("Done processing a packet/packets\n");
+#endif
 	LoRa_Start_Receive();
+}
+
+void Process_Packet() {
+	while (indx_2 >= 2) {
+		uint8_t ptype = rx_final_buffer[0];
+		uint8_t payload_length = rx_final_buffer[1];
+		uint16_t total_packet_size = OPCODE_OFFSET + LENGTH_OFFSET + payload_length;
+
+		if (indx_2 >= total_packet_size) {
+			Receive_Packet_Handler(rx_final_buffer, total_packet_size, ptype);
+
+			if (indx_2 > total_packet_size) {
+				memmove(rx_final_buffer, rx_final_buffer + total_packet_size,
+						indx_2 - total_packet_size);
+				indx_2 -= total_packet_size;
+			} else {
+				indx_2 = 0;
+			}
+		} else {
+			break;
+		}
+	}
+}
+
+void Process_LoRa_Reply() {
+	while (indx_2 >= 3) {  // Must be: C1 | STARTING_ADDRESS | LENGTH
+		if (rx_final_buffer[0] != 0xC1) {
+#ifdef DEBUG
+			printf("A LoRa reply had \"0x%02X\" as its reply code\n", rx_final_buffer[0]);
+#endif
+			memmove(rx_final_buffer, rx_final_buffer + 1, indx_2 - 1);
+			indx_2--;
+			continue;
+		}
+
+		uint8_t starting_address = rx_final_buffer[1];
+		uint8_t data_length = rx_final_buffer[2];
+		uint16_t total_reply_size = 3 + data_length;
+		if (indx_2 >= total_reply_size) {
+			LoRa_Reply_Handler(rx_final_buffer, starting_address, data_length);
+
+			if (indx_2 > total_reply_size) {
+				memmove(rx_final_buffer, rx_final_buffer + total_reply_size, indx_2 - total_reply_size);
+				indx_2 -= total_reply_size;
+			} else {
+				indx_2 = 0;
+			}
+		} else {
+			break;
+		}
+	}
+}
+
+void LoRa_Reply_Handler(uint8_t rx_final_buffer[], uint8_t starting_address, uint8_t data_length) {
+	receiving_config_data = false;
+
+	if (starting_address == PID) {
+		if (data_length != 7)
+			return;
+		for (int i = 0; i < 6; i++)
+			printf("0x%02X ", rx_final_buffer[i]);
+		return;
+	}
+
+	uint8_t current_data;
+
+	for (int i = 0; i < data_length; i++) {
+		current_data = rx_final_buffer[3 + i];
+		switch (starting_address + i) {
+		case ADDH:
+			addh = current_data;
+			break;
+		case ADDL:
+			addl = current_data;
+			break;
+		case NETID:
+			netid = current_data;
+			break;
+		case REG0:
+			reg0 = current_data;
+			break;
+		case REG1:
+			reg1 = current_data;
+			break;
+		case REG2:
+			reg2 = current_data;
+			break;
+		case REG3:
+			reg3 = current_data;
+			break;
+		default:
+			break;
+		}
+	}
 }
--- LoRaMesh/Src/Mesh.c
+++ LoRaMesh/Src/Mesh.c
@@ -13,14 +13,14 @@
 RTC_TimeTypeDef currentTime;
 RTC_DateTypeDef currentDate;
 
-static uint8_t tx_data_to_send[TX_SIZE] = { 0 };
+static uint32_t previous_timestamp = 0;
+
 static uint8_t tx_buffer[TX_SIZE] = { 0 };
 static uint8_t tx_byte;
 static uint8_t tx_index = 0;
 
 static uint32_t my_sequence_number;
 static uint32_t rreq_id;
-uint32_t unicast_entries;
 struct unicast_route_table_entry unicast_route_table[UNICAST_TABLE_LENGTH];
 struct noroute_table_entry noroute_table[NOROUTE_QUEUE_MAX_ENTRIES];
 uint8_t noroute_table_entries;
@@ -46,7 +46,6 @@ void Mesh_Init() {
 
 	Generate_RREQ_ID();
 	my_sequence_number = 0;
-	Update_Route_Table(my_id, my_sequence_number, 0, my_id);
 	printf("Node ID: %d\n\n", my_id);
 #ifdef DEBUG
 	printf("Sequence Number: %ld, RREQ ID: %" PRIu32 "\n", my_sequence_number, rreq_id);
@@ -57,26 +56,40 @@ void Mesh_Init() {
 #endif
 }
 
-bool Mesh_Transmit(uint16_t destination_id, uint8_t data[],
-		uint8_t data_length) {
+bool Mesh_Transmit(uint16_t destination_id, uint8_t data[], uint8_t data_length) {
+	bool isPing = false;
 
 	if (my_id == destination_id) {
 		printf("Sending a message to yourself? Abort\n");
 		return SUCCESS;
+	}
+
+	if (data_length == 4 && memcmp(data, "ping", 4) == 0) {
+#ifdef DEBUG
+	printf("Handling a ping packet\n");
+#endif
+	    isPing = true;
 	}
 #ifdef DEBUG
 	printf("Sending data over the Mesh\n");
 #endif
 
 	int8_t route_idx = Route_Exists(destination_id);
+	Update_Routes_Expiration();
 	if (route_idx != -1) {
-		printf("Route is known. Sending data directly\n");
+#ifdef DEBUG
+		printf("Route is known. Sending packet directly\n");
+#endif
 		struct unicast_route_table_entry route;
 		route = unicast_route_table[route_idx];
-		Mesh_Send_Data(route.destination_id, route.destination_sequence_number,
-				data, route.next_hop_destination_id, 0, my_id, 0, data_length);
+		if (isPing) {
+			Mesh_Send_PING(route.next_hop_destination_id, route.destination_id, my_id, 0, PING_REQUEST, Get_Timestamp());
+		} else
+			Mesh_Send_Data(route.destination_id, route.destination_sequence_number, data, route.next_hop_destination_id, 0, my_id, 0, data_length);
 	} else {
+#ifdef DEBUG
 		printf("Route is not known. Sending a RREQ packet\n");
+#endif
 
 		Generate_RREQ_ID();
 		Mesh_Send_RREQ(destination_id, my_id, my_sequence_number, 0, rreq_id);
@@ -84,13 +97,24 @@ bool Mesh_Transmit(uint16_t destination_id, uint8_t data[],
 #ifdef DEBUG
 		printf("Adding the request to the no-route table\n");
 #endif
-		noroute_table[noroute_table_entries].destination_id = destination_id;
-		noroute_table[noroute_table_entries].data = data;
-		noroute_table[noroute_table_entries].data_length = data_length;
-		noroute_table_entries++;
-		if (noroute_table_entries == NOROUTE_QUEUE_MAX_ENTRIES)
-			noroute_table_entries = 0;
+		Noroute_Table_Add(destination_id, data, data_length);
 	}
+	return SUCCESS;
+}
+
+bool Noroute_Table_Add(uint16_t destination_id, uint8_t data[], uint8_t data_length) {
+
+    if (data_length > MAX_PAYLOAD_SIZE) {
+    	printf("Data was too large (%d byte) to be added to no-route table. Max length: %d\n", data_length, MAX_PAYLOAD_SIZE);
+        return FAIL;
+    }
+
+	noroute_table[noroute_table_entries].destination_id = destination_id;
+	memcpy(noroute_table[noroute_table_entries].data, data, data_length);
+	noroute_table[noroute_table_entries].data_length = data_length;
+	noroute_table_entries++;
+	if (noroute_table_entries == NOROUTE_QUEUE_MAX_ENTRIES)
+		noroute_table_entries = 0;
 	return SUCCESS;
 }
 
@@ -104,8 +128,8 @@ int8_t Route_Exists(uint16_t id) {
 #ifdef DEBUG
 	printf("\tLooking for id = %d in unicast route table\n", id);
 #endif
-	for (int i = 0; i < unicast_entries; i++) {
-		if (unicast_route_table[i].destination_id == id) {
+	for (int i = 0; i < UNICAST_TABLE_LENGTH; i++) {
+		if (unicast_route_table[i].destination_id == id && unicast_route_table[i].expiration_time > 0) {
 #ifdef DEBUG
 			printf("\tFound entry for id=%d, next node is %d\n", unicast_route_table[i].destination_id, unicast_route_table[i].next_hop_destination_id);
 #endif
@@ -146,14 +170,12 @@ bool Update_Route_Table(uint16_t dest_id, uint32_t dest_seq_num,
 #ifdef DEBUG
 		printf("\tSource node not found in table. Adding.\n");
 #endif
-		unicast_route_table[unicast_entries].destination_id = dest_id;
-		unicast_route_table[unicast_entries].destination_sequence_number = dest_seq_num;
-		unicast_route_table[unicast_entries].hop_count = num_hops;
-		unicast_route_table[unicast_entries].next_hop_destination_id = next_hop;
-		unicast_route_table[unicast_entries].expiration_time = DEFAULT_ROUTE_EXPIRATION_TIME;
-		unicast_entries++;
-		if (unicast_entries == UNICAST_TABLE_LENGTH)
-			unicast_entries = 0;
+		uint8_t replacement_route = Update_Routes_Expiration();
+		unicast_route_table[replacement_route].destination_id = dest_id;
+		unicast_route_table[replacement_route].destination_sequence_number = dest_seq_num;
+		unicast_route_table[replacement_route].hop_count = num_hops;
+		unicast_route_table[replacement_route].next_hop_destination_id = next_hop;
+		unicast_route_table[replacement_route].expiration_time = DEFAULT_ROUTE_EXPIRATION_TIME;
 	} else {
 		struct unicast_route_table_entry *existing_route = &unicast_route_table[source_idx];
 
@@ -182,6 +204,41 @@ bool Update_Route_Table(uint16_t dest_id, uint32_t dest_seq_num,
 	return SUCCESS;
 }
 
+uint8_t Update_Routes_Expiration() { //Also returns id of the least fresh one
+#ifdef DEBUG
+	printf("Updating each route's freshness\n");
+#endif
+	uint32_t new_timestamp = Get_Timestamp() / 1000;
+	uint32_t difference = 0;
+	if (new_timestamp < previous_timestamp) //overflow control
+		difference = new_timestamp;
+	else
+		difference = new_timestamp - previous_timestamp;
+	previous_timestamp = new_timestamp;
+
+	uint8_t route_idx = 0;
+	uint8_t lowest_freshness = unicast_route_table[0].expiration_time;
+
+	for (int i = 0; i < UNICAST_TABLE_LENGTH; i++) {
+		if (unicast_route_table[i].expiration_time <= difference) { //overflow control
+			unicast_route_table[i].expiration_time = 0;
+			route_idx = i;
+			lowest_freshness = 0;
+		} else {
+			unicast_route_table[i].expiration_time = unicast_route_table[i].expiration_time - previous_timestamp;
+			if (unicast_route_table[i].expiration_time < lowest_freshness) {
+				route_idx = i;
+				lowest_freshness = unicast_route_table[i].expiration_time;
+			}
+		}
+	}
+#ifdef DEBUG
+	printf("Returning route id: %d, time until expiration: %" PRIu32 "\n", route_idx, unicast_route_table[route_idx].expiration_time);
+#endif
+
+	return route_idx;
+}
+
 void Generate_RREQ_ID(void) {
 	HAL_RTC_GetTime(Mesh_RTC, &currentTime, RTC_FORMAT_BIN);
 	HAL_RTC_GetDate(Mesh_RTC, &currentDate, RTC_FORMAT_BIN);
@@ -202,21 +259,48 @@ bool Is_Fresher_Route(uint32_t new_seq, uint32_t old_seq) {
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
 	if (huart->Instance == COM_UART->Instance) {
 
-		uint32_t target_address = 0;
-		tx_buffer[tx_index] = tx_byte;
-		if (++tx_index >= TX_SIZE)
-			tx_index = 0;
+//		uint32_t target_address = 0;
+//		tx_buffer[tx_index] = tx_byte;
+//		if (++tx_index >= TX_SIZE)
+//			tx_index = 0;
+//
+//		if (tx_index > 2)
+//			target_address = tx_buffer[0] - '0';
+//
+//		if (tx_byte == '\n' || tx_byte == '\r') {
+//			memset(tx_data_to_send, 0, TX_SIZE);
+//			memcpy(tx_data_to_send, tx_buffer, tx_index);
+//			memset(tx_buffer, 0, TX_SIZE);
+//			Mesh_Transmit(target_address, &tx_data_to_send[2], tx_index - 2);
+//			tx_index = 0;
+//		}
 
-		if (tx_index > 2)
-			target_address = tx_buffer[0] - '0';
+		tx_buffer[tx_index] = tx_byte;
 
 		if (tx_byte == '\n' || tx_byte == '\r') {
-			memset(tx_data_to_send, 0, TX_SIZE);
-			memcpy(tx_data_to_send, tx_buffer, tx_index);
+			uint16_t dest_id = 0;
+			uint8_t data_length = 0;
+			char *space_pos = strchr((char*) tx_buffer, ' ');
+
+			if (space_pos != NULL) {
+				dest_id = atoi((char*) tx_buffer);
+
+				uint8_t data_start = (space_pos - (char*) tx_buffer) + 1;
+				data_length = tx_index - data_start;
+
+				Mesh_Transmit(dest_id, &tx_buffer[data_start], data_length);
+			}
+
+			// Reset buffer and index
 			memset(tx_buffer, 0, TX_SIZE);
-			Mesh_Transmit(target_address, &tx_data_to_send[2], tx_index - 2);
 			tx_index = 0;
+		} else {
+			// Increment index and wrap around if needed
+			if (++tx_index >= TX_SIZE) {
+				tx_index = 0;
+			}
 		}
+
 		HAL_UART_Receive_IT(COM_UART, &tx_byte, 1);
 	}
 }
